7a8ad10178c985b7b8b99184dedd80a76b0ecb55
sinmsinm <alex.balleste@gmail.com>
2015-01-27 17:57:01 +0100

    Reenable all videos when a window is selected. Fixed #64

--- client/js/services/windowhandler.js
+++ client/js/services/windowhandler.js
@@ -65,12 +65,21 @@ angular.module('mean.rooms').factory("WindowHandler",[function(){
 				setTimeout(function() {selWindow();}, 100);
 			};
 			
+				var selWindow = function (win){
+					//There is a perverse effect on window selection that pauses the video, lets play all 
+					var videos = document.getElementsByTagName('video');
+					for (i = 0; i< videos.length; i++){
+						videos[i].play();	
+					}
+				};
+			
 			var window = {
 				options: options,
 				title: winTitle,
 				mediaElement: mediaElement,
 				close: close,
 				closeable: (closeable===undefined) ? false : closeable,
+				selectwindow: selWindow,
 				open: open,
 			};
 			
