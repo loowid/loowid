ed11cedf697924bb3d58cd0f1300f242d56c3e20
juanjmerono <jjmerono@um.es>
2015-02-21 21:58:46 +0100

    Remove unused directive.

--- client/js/app.js
+++ client/js/app.js
@@ -3,7 +3,7 @@
 window.app = angular.module('mean', ['ngCookies', 'ngResource', 'ngRoute', 'mean.system', 'mean.rooms', 'mean.stats', 'ngSanitize','ngAnimate','ngI18n','ngTagsInput','ngWindowManager','notification']);
 
 angular.module('mean.system', []);
-angular.module('mean.rooms', ['luegg.directives','chart.js']);
+angular.module('mean.rooms', ['luegg.directives']);
 angular.module('mean.stats', ['chart.js']);
 
 
