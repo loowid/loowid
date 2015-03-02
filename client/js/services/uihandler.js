2966086a674f7213ef964d3952768b0c97f523ad
juanjmerono <jjmerono@um.es>
2015-03-02 18:21:47 +0100

    More friendly UX with share loowid links.

--- client/js/services/uihandler.js
+++ client/js/services/uihandler.js
@@ -58,40 +58,58 @@ angular.module('mean.rooms').factory('UIHandler',['$window','$timeout',function(
 				window.clearInterval(rAddThis);
 				_this._data.addThisParent = addThis.parentNode;
 				_this._data.addThisNode = addThis;
-				addThis.parentNode.removeChild(addThis);   
+				addThis.parentNode.removeChild(addThis);
 			}
 	   	},100);
 	};
 	
-	_this._data.addThisSetUrl = function($scope,pm) {
+	_this._data.addThisSetUrl = function($scope) {
 		var addthis = 'addthis_share';
-		var mtitle = 'addthistitle';
 		window[addthis] = {};
-		_this._data.sharepermanent = pm && _this._data.sharepermanent;
 		if (_this._data.sharepermanent) {
-			mtitle = 'addthisptitle';
+			window[addthis].title = $scope.resourceBundle.addthisptitle;
 			window[addthis].url = _this._data.permanenturl;
 		} else {
+			window[addthis].title = $scope.resourceBundle.addthistitle;
 			window[addthis].url = _this._data.screenurl;
 		}
-		var itm = setTimeout(function(){
-			if ($scope.resourceBundle) {
-				window[addthis].title = $scope.resourceBundle[mtitle];
-				window.clearInterval(itm);
-			}
-		},100);
+	};
+	
+	_this._data.addThisSlideOut = function() {
+		if (_this._data.addthisslide) {
+			window.clearTimeout(_this._data.addthisslide);
+		}
+		_this._data.addthisslide = setTimeout(function(){
+			document.getElementById('at4-scc').click();
+			_this._data.addthisvisible = false;
+		},10000);
 	};
 	
 	_this._data.showAddThis = function($scope) {
 		_this._data.addThisSetUrl($scope);
-		if (_this._data.addThisParent) {
+		var ats = document.getElementById('at4-share');
+		if (!ats) {
+			ats = _this._data.addThisNode;
 			_this._data.addThisParent.appendChild(_this._data.addThisNode);
+			_this._data.addThisSlideOut();
+		} else {
+			if (ats.className.indexOf('at4-show')>0) {
+				_this._data.addThisSlideOut();
+			} else {
+				document.getElementById('at4-soc').click();
+				_this._data.addThisSlideOut();
+			}
+		}
+		if (_this._data.sharepermanent && ats.className.indexOf('atss-perm')<0) {
+			ats.className += ' atss-perm';
+		} else {
+			ats.className = ats.className.replace(' atss-perm','');
 		}
 	};
 
 	_this._data.sharePermanentUrl = function(value,$scope) {
 		_this._data.sharepermanent = value;
-		_this._data.addThisSetUrl($scope,true);
+		_this._data.showAddThis($scope);
 		
 	};
 	
